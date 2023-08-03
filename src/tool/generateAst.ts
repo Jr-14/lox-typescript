44252364200486c29e9f424132653de8c3860c60
John Requizo <jereq1@student.monash.edu>
2023-08-03 21:20:08 +1000

    Add define ast for astprinter - won't use

--- src/tool/generateAst.ts
+++ src/tool/generateAst.ts
@@ -1,4 +1,5 @@
 import process from 'node:process';
+import fs from 'node:fs';
 
 const generateAst = (args: string[]) => {
     if (args.length != 3) {
@@ -6,4 +7,25 @@ const generateAst = (args: string[]) => {
         process.exit(9);
     }
     const outputDir = args[3];
+    defineAst(outputDir, "Expr", [
+        "Binary   : Expr left, Token operator, Expr right",
+        "Grouping : Expr expression",
+        "Literal  : Object value",
+        "Unary    : Token operator, Expr right"
+    ]);
+}
+
+const defineAst = (
+    outputDir: string,
+    baseName: string,
+    types: string[]
+) => {
+    const path = outputDir + "/" + baseName + ".ts";
+    const content = "lox-typescript\n";
+    fs.writeFileSync(path, content);
+    for (const type of types) {
+        const className = type.split(':')[0].trim();
+        const fields = type.split(':')[1].trim();
+        defineType(baseName, className, fields);
+    }
 }
